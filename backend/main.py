c2ea4c32467c07ce7e04bc05d0a7161bbc1e82d3
tvasquezm <tvasquezm@utem.cl>
2026-06-26 20:06:31 -0400

    fix main.py

--- backend/main.py
+++ backend/main.py
@@ -1672,4 +1672,6 @@ async def chat(payload: ChatRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 9000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
