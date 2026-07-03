932f35717c64930c9396242ce61dda1a5e93ef09
LucianoCon <lconchac@utem.cl>
2026-07-03 19:35:09 -0400

    prueba commit

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -144,8 +144,6 @@ const TopBar: React.FC = () => {
             <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83-2.83l.06.06A1.65 1.65 0 0 0 9 4.68a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 2.83l-.06.06A1.65 1.65 0 0 0 19.4 9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z" />
           </svg>
         </button>
-
-        {/* 🔥 NUEVO: Botón de Ayuda contextual */}
         <button className="icon-btn" onClick={() => setHelpOpen(true)} title="Guía de esta pantalla">
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
             <circle cx="12" cy="12" r="10"></circle>
@@ -167,7 +165,6 @@ const TopBar: React.FC = () => {
 
       <SettingsModal isOpen={settingsOpen} onClose={() => setSettingsOpen(false)} />
       
-      {/* 🔥 NUEVO: Renderizamos el modal pasándole el título y contenido */}
       <HelpModal 
         isOpen={helpOpen} 
         onClose={() => setHelpOpen(false)} 
